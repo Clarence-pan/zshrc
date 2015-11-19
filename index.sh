b73250ab9f5b65902b4e7c5368613b4cb8d2e117
Clarence <clarence-pan@foxmail.com>
2015-11-19 14:01:28 +0800

    add some aliases

--- index.sh
+++ index.sh
@@ -1,3 +1,7 @@
 export PATH=$PATH:~/bin
 export prompt="%n@%m$prompt"
 
+alias use-socks-proxy='source ~/bin/use-socks-proxy'
+alias use-http-proxy='source ~/bin/use-http-proxy'
+alias composer='composer.phar'
+
