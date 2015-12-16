03165b14244329a5887e87a7a3cee81ab64fed81
Clarence <clarence-pan@foxmail.com>
2015-12-16 16:38:17 +0800

    auto-backup

--- index.sh
+++ index.sh
@@ -4,4 +4,5 @@ export prompt="%n@%m$prompt"
 alias use-socks-proxy='source ~/bin/use-socks-proxy'
 alias use-http-proxy='source ~/bin/use-http-proxy'
 alias composer='composer.phar'
+export EDITOR=e
 
