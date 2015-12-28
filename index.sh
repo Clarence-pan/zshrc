66f9b34340260e222af84cdfd02f6ca77ae972cf
Clarence <clarence-pan@foxmail.com>
2015-12-28 17:30:00 +0800

    auto-backup

--- index.sh
+++ index.sh
@@ -5,5 +5,5 @@ alias help='tldr'
 alias use-socks-proxy='source ~/bin/use-socks-proxy'
 alias use-http-proxy='source ~/bin/use-http-proxy'
 alias composer='composer.phar'
-export EDITOR=e
+export EDITOR=vim
 
