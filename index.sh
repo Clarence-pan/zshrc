533b16b1abcc05a085a6b677724443fa474513f3
Clarence <clarence-pan@foxmail.com>
2016-01-05 17:30:01 +0800

    auto-backup

--- index.sh
+++ index.sh
@@ -5,5 +5,11 @@ alias help='tldr'
 alias use-socks-proxy='source ~/bin/use-socks-proxy'
 alias use-http-proxy='source ~/bin/use-http-proxy'
 alias composer='composer.phar'
-export EDITOR=vim
+
+# Preferred editor for local and remote sessions
+if [[ -n $SSH_CONNECTION ]]; then
+    export EDITOR='vim'
+else
+    export EDITOR='emacsclient --alternate-editor=vim'
+fi
 
