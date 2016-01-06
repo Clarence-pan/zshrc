efbad5451cbf452c835f5dba7cf503a00fdf44e0
Clarence <clarence-pan@foxmail.com>
2016-01-06 11:30:00 +0800

    auto-backup

--- index.sh
+++ index.sh
@@ -5,6 +5,7 @@ alias help='tldr'
 alias use-socks-proxy='source ~/bin/use-socks-proxy'
 alias use-http-proxy='source ~/bin/use-http-proxy'
 alias composer='composer.phar'
+alias emacs='/Applications/Emacs.app/Contents/MacOS/Emacs-x86_64-10_9'
 
 # Preferred editor for local and remote sessions
 if [[ -n $SSH_CONNECTION ]]; then
