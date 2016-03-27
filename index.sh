76ff3e30eba3197b928c843328c4c18bb6522647
Clarence <clarence-pan@foxmail.com>
2016-03-27 17:30:01 +0800

    auto-backup

--- index.sh
+++ index.sh
@@ -9,6 +9,7 @@ alias use-http-proxy='source ~/bin/use-http-proxy'
 
 alias emacs='/Applications/Emacs.app/Contents/MacOS/Emacs'
 #alias emacs='/usr/local/Cellar/emacs-mac/emacs-24.5-z-mac-5.15/Emacs.app/Contents/MacOS/Emacs'
+alias git-pull-all="git checkout develop && git pull origin develop && git checkout test && git pull origin test"
 
 # Preferred editor for local and remote sessions
 if [[ -n $SSH_CONNECTION ]]; then
